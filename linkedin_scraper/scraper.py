24fe37b93f628d416d4bd77a26791454ee28efbe
BohdanDuma <duma-bohdan@ukr.net>
2026-04-30 16:10:22 +0300

    class structure

--- linkedin_scraper/scraper.py
+++ linkedin_scraper/scraper.py
@@ -8,15 +8,18 @@ from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
 from pathlib import Path
-env_file = Path(__file__).resolve().parent.parent / '.env'
+class YoutubeLoader:
+    def __init__(self):
+        self.env_file = Path(__file__).resolve().parent.parent / '.env'
 
-if env_file.exists():
-    print('exict')
+        def _connection(self):
+            if self.env_file.exists():
+                print('exict')
 
-load_dotenv(dotenv_path=env_file)
-API_KEY=os.getenv('YOUTUBE_MY_API_KEY')
-try:
-    service = build('youtube','v3', developerKey=API_KEY) 
-    print(service)
-except Exception as e:
-    print(e)
+            load_dotenv(dotenv_path=self.env_file)
+            API_KEY=os.getenv('YOUTUBE_MY_API_KEY')
+            try:
+                self.service = build('youtube','v3', developerKey=API_KEY) 
+                logging.info('Youtube service connected')
+            except Exception as e:
+                print(e)
