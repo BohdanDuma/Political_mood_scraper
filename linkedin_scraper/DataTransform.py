9a2f0d54866e1f8464acc438ecc0b3bb9d9ecf73
BohdanDuma <duma-bohdan@ukr.net>
2026-05-05 16:11:06 +0300

    added df pandas transformation logic

--- linkedin_scraper/DataTransform.py
+++ linkedin_scraper/DataTransform.py
@@ -1,6 +1,7 @@
 
 import requests
 from parsel import Selector 
+import pandas as pd
 import sys
 import logging 
 import json
@@ -9,6 +10,7 @@ from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import jmespath
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s%(levelname)s%(message)s',
@@ -18,13 +20,21 @@ logging.basicConfig(
     ]
 )
 with open('raw_comments.json', 'r', encoding='utf-8') as f:
-               data = json.loads(f)
+               data = json.load(f)
 
 class DataTransformer:
     def __init__(self, raw_data):
         self.raw_data = raw_data
         self._base()
     def _base(self):
+            #тут використати import jmespath для того щоб витянути ті дані які потрібно
+            try:
+                jms_data = jmespath.search("items[*].{comment_id: id, author_name: snippet.topLevelComment.snippet.authorDisplayName, author_id: snippet.topLevelComment.snippet.authorChannelId.value, text: snippet.topLevelComment.snippet.textDisplay, likes: snippet.topLevelComment.snippet.likeCount, published_at: snippet.topLevelComment.snippet.publishedAt, updated_at: snippet.topLevelComment.snippet.updatedAt}", self.raw_data)
+                df = pd.DataFrame(jms_data)
+                print(df['likes'].notna().count())
+            except Exception as e:
+                   logging.error('Error transformation with JMES')
             
-            print(self.raw_data)
+            #далі добавити трансформацію в датафрейм
+            
 new = DataTransformer(data)
