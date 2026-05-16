09b76e20d5a791b781246275722d76e7f5c913e8
BohdanDuma <duma-bohdan@ukr.net>
2026-05-16 23:29:42 +0300

    added sql method

--- linkedin_scraper/export.py
+++ linkedin_scraper/export.py
@@ -10,20 +10,23 @@ try:
 except:
     print('couldnt connect to db')
     '''
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 class Datebase:
-    def __init__(self,df):
+    def __init__(self,df,db_path):
         self.df = df
-    def stat_calc(self):
-        self.df     
-    def save_to_db(db):
-        engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-        texts = db['text'].astype(str).tolist()
-        print('done')
-    def get_last_sycn(self):
-        pass
+        self.engine = create_engine("sqlite+pysqlite:///{db_path}", echo=True)
+        self.create_tables()
+    def create_tables(self):
+        query = 'CREATE TABLE IF NOT EXISTS video_stats (video_id TEXT PRIMARY KEY, video_title TEXT, pos_count INTEGER DEFAULT 0, neg_count INTEGER DEFAULT 0,neu_count INTEGER DEFAULT 0, total_likes INTEGER DEFAULT 0, last_sync_date DATETIME);'    
+        with self.engine.connect() as conn:
+            conn.execute(text(query))
+            conn.commit()
+    def get_last_sycn(self, video_id):
+        query = text('SELECT last_sync_date FROM video_stats WHERE video_id=:vid')
+        with self.engine.connect() as conn:
+            result = conn.execute(query, {"vid": video_id}).fetchone()
+            return result[0] if result else "1900-01-01T00:00:00:00Z"
     def update_video_stats(self):
         pass
     
-save_to_db()
