a10bb65dd0d9ffcbe87b310107b5382bb30b340c
BohdanDuma <duma-bohdan@ukr.net>
2026-05-26 22:22:06 +0300

    fixed/ YouTubeLoader class

--- linkedin_scraper/YouTubeLoader.py
+++ linkedin_scraper/YouTubeLoader.py
@@ -25,41 +25,64 @@ class YoutubeLoader:
             logging.error(f'{e} YouTube not connection ')
             logging.info('Youtube service connected')
         
-    def fetch_comment(self, video_id):
+    def fetch_comment(self, video_id, last_fetched=None, max_pages=3):
+        #if there are no comments from this video in the database
+        cache_path = Path(f'cache_{video_id}.parquet')
+        if cache_path.exists():
+            print(f"Download Paruet-cache to: {cache_path}")
+            return pd.read_parquet(cache_path)
+        if last_fetched is None:
+            last_fetched = "1900-01-01T00:00:00Z"
+        #if there are no response from API 
         if not self.service:
             logging.error("YouTube service not connected")
             return pd.DataFrame()
         new_comments = []
-        next_page_tosken = None
-
-        while True:
+        next_page_token=None
+        pages_processed = 0 
+        while pages_processed < max_pages:
             try:
-                cache_file = Path(f'cache_{video_id}.json')
-                if cache_file.exists():
-                    with open(cache_file,'r', encoding="utf-8") as f:
-                        return json.load(f)
                 request = self.service.commentThreads().list(
         part="snippet",
         videoId=video_id,
         maxResults=100,
         order='time',
         textFormat="plainText",
-        pageToken=next_page_tosken
+        pageToken=next_page_token
     )
                 response = request.execute()
-                with open(cache_file, 'w', encoding="utf-8") as f:
-                    json.dump(response,f,indent=4,ensure_ascii=False)
+                
+                items = response.get('items',[])
+
+                if not items: break
                 stop_fetching = False
-
-                '''items = response.get('items',[])
-
-                if not items:
-                    break
                 for item in items:
-                    snippet = item['topLevelComment']['snippet']
-                print(items)'''
-                return response
+                    snippet = item['snippet']['topLevelComment']['snippet']
+                    published_at = snippet['publishedAt']
+                    if published_at > last_fetched:
+                        new_comments.append({
+                            'comment_id': item['id'],
+                            'text': snippet['textDisplay'],
+                            'likes': snippet['likeCount'],
+                            'published_at': published_at
+                        })
+                    else:
+                        stop_fetching = True
+                        break
+                if stop_fetching:
+                    break        
+                next_page_token= response.get('nextPageToken')
+                if not next_page_token:
+                    break
+                #add count to processed pages
+                pages_processed += 1
             except Exception as e:
-                print('promlem')
+                print(f'API Error: {e}')
+        df_final = pd.DataFrame(new_comments)
+        if not df_final.empty:
+            df_final.to_parquet(cache_path,index=False)
+            print(f'DataFrame saved to Parquet')
+        return df_final    
 new_ex = YoutubeLoader()
-new_ex.fetch_comment('neYVUCDg100' )
+res = new_ex.fetch_comment('neYVUCDg100' )
+print(res)
